088b29abb01cda9fc7d7416217aea26b3ea5df1f
funmi <tope@zabira.ng>
2023-01-03 05:29:59 +0100

    trying out other string

--- variables.go
+++ variables.go
@@ -14,4 +14,26 @@ func main() {
 	usingCreateAssigned := "holla funmi"
 
 	fmt.Println("what do you say", usingCreateAssigned)
+
+	var sum int
+	fmt.Println("the total sumis : ", sum)
+
+	part1, other := 1, 2
+
+	fmt.Println("this is a part one var", part1, "\n this is other var : ", other)
+
+	part2, other := 4, 5
+
+	fmt.Println("part two:", part2, "this is reassigned other val : ", other)
+
+	sum = part1 + part2 + other
+
+	fmt.Println("the total sum: ", sum)
+
+	var (
+		testingBlock1 = 3
+		testingBlock2 = 9
+	)
+
+	fmt.Println("logging testingBlock1: ", testingBlock1, " AND Logging TestingBlock2::", testingBlock2)
 }
