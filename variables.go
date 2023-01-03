54d322af13176812f21dc38ff053d92785fb6dd4
funmi <tope@zabira.ng>
2023-01-03 05:00:21 +0100

    trying out other string

--- variables.go
+++ variables.go
@@ -7,4 +7,11 @@ func main() {
 	var myName = "jake-flip flop"
 
 	fmt.Println("my name is", myName)
+
+	var flexName string = "flex name is flex name"
+	fmt.Println("excuse me :", flexName)
+
+	usingCreateAssigned := "holla funmi"
+
+	fmt.Println("what do you say", usingCreateAssigned)
 }
